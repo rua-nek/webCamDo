0b858185c5e16f737be12eb2be31403a7e6e97a8
tui la rua <125947297+rua-nek@users.noreply.github.com>
2024-12-22 20:00:41 +0700

    ...

--- src/app/controllers/dataAggregatorController.js
+++ src/app/controllers/dataAggregatorController.js
@@ -2,6 +2,9 @@ class dataAggregatorController {
     index(req, res) {
         res.render('dataAggregator');
     }
+    home(req, res) {
+        res.render('home');
+    }
 }
 
 module.exports = new dataAggregatorController();
